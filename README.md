5878f4751606b9a8d0e5a58f3eb5bfb03197f640
benjaminsimier <benjaminsimier@gmail.com>
2022-09-10 14:25:21 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WordPress-Gulp.js
 My Gulp.js file included sass, css minification, js minification, and Livereload
+
+## How to use
+- Add package.json + gulpfile.js in your WordPress theme
+- npm install
+- Install the Livereload Chrome plugin - https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
+- Run: gulp watch
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+var autoprefixer = require('gulp-autoprefixer');
+
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var babel = require('gulp-babel');
+
+var rename = require('gulp-rename');
+
+var livereload = require('gulp-livereload');
+
+var autoprefixBrowsers = [
+  '> 1%', 
+  'last 2 versions', 
+  'firefox >= 4', 
+  'safari 7', 'safari 8', 
+  'IE 8', 'IE 9', 'IE 10', 'IE 11'
+];
+ 
+// SASS + MIN CSS
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ 
+      overrideBrowserslist: autoprefixBrowsers 
+    }))
+    .pipe(gulp.dest('./'))
+
+    .pipe(cleanCSS({
+      compatibility: 'ie8',
+      level: {
+        1: {
+          specialComments: 0
+        }
+      }
+    }))
+    .pipe(rename({ suffix: '.min' }))
+
+    .pipe(gulp.dest('./'))
+
+    .pipe(livereload());
+});
+
+// MIN JAVASCRIPT 
+gulp.task('js', function() {
+  return gulp.src('./js/!(*.min)*.js')
+  // return gulp.src('./js/app.js')
+
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/js'))
+
+    .pipe(livereload({
+      // basePath: './dist/js/app.min.js'
+    }));
+});
+
+// WATCH
+gulp.task('watch', function() {
+  livereload.listen({
+    // port: 3005 // You can change the port here
+  });
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('./js/!(*.min)*.js', gulp.series('js'));
+});
